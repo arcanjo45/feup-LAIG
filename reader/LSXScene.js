3fa6c2a504b1075b7f8068379e6ee3c55cdfbdd1
Pedro <pedro.mvsilva@gmail.com>
2015-10-13 16:27:18 +0100

    Luzes a pintar
    top

--- reader/LSXScene.js
+++ reader/LSXScene.js
@@ -49,6 +49,8 @@ LSXScene.prototype.onGraphLoaded = function()
 
 	 this.applyInitials();
 
+	 this.initLights();
+
 
 	 this.axis = new CGFaxis(this,this.graph.initials.reference);
 
@@ -67,9 +69,63 @@ LSXScene.prototype.applyInitials = function(){
 
 var iniciais = this.graph.initials;
 
+var trans = iniciais.translation;
+
+var rots = iniciais.rotations;
+
+var scal = iniciais.scale;
+
+this.translate(trans.x,trans.y,trans.z);
+
+for(var i=0; i < rots.length;i++)
+{
+	if(rots[i].axis == 'x')
+		this.rotate(rots[i].angle*deg2rad,1,0,0);
+	
+	if(rots[i].axis == 'y')
+		this.rotate(rots[i].angle*deg2rad,0,1,0);
+		
+	if(rots[i].axis == 'z')
+		this.rotate(rots[i].angle*deg2rad,0,0,1);
+
+}
+
+this.scale(scal.sx,scal.sy,scal.sz);
+
+console.log(iniciais.translation);
+
+
+};
+
+LSXScene.prototype.initLights = function(){
+
+	this.shader.bind();
+
+	this.luzes = [];
+
+	for(var i =0; i < this.graph.lights.length;i++)
+	{
+		var luz = this.graph.lights[i];
+
+		if(luz.enabled)
+			this.lights[i].enable();
+		else
+			this.lights[i].disable();
+
+		this.lights[i].setPosition(luz.position.x,luz.position.y,luz.position.z);
+		this.lights[i].setAmbient(luz.ambient.r,luz.ambient.g,luz.ambient.b,luz.ambient.a);
+		this.lights[i].setDiffuse(luz.diffuse.r,luz.diffuse.g,luz.diffuse.b,luz.diffuse.a);
+		this.lights[i].setSpecular(luz.specular.r,luz.specular.g,luz.specular.b,luz.specular.a);
+		this.lights[i].setVisible(true);
+		this.lights[i].update();
+
+		//aux = this.lights[i];
+	}
 
 
 
+
+  this.shader.unbind();
 };
 
 LSXScene.prototype.display = function() {
@@ -85,6 +141,9 @@ LSXScene.prototype.display = function() {
       this.applyViewMatrix();
 
       this.axis.display();
+
+      for (var i = 0; i < this.lights.length; i++)
+            this.lights[i].update();
 
 this.shader.unbind();
 };
