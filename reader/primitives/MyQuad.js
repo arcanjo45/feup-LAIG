be07257672469d52f54d15a5202bd321efbd5ec9
Luís Oliveira <luisoliveira8c@gmail.com>
2015-10-17 02:16:21 +0100

    corrigida aplicação da textura no myQuad

--- reader/primitives/MyQuad.js
+++ reader/primitives/MyQuad.js
@@ -1,55 +1,54 @@
 
- function MyQuad(scene,args) {
- 	CGFobject.call(this,scene);
+function MyQuad(scene,args) {
+    CGFobject.call(this,scene);
 
 
- 	this.args=args;
+    this.args=args;
 
- 	this.initBuffers();
- };
+    this.initBuffers();
+};
 
- MyQuad.prototype = Object.create(CGFobject.prototype);
- MyQuad.prototype.constructor = MyQuad;
+MyQuad.prototype = Object.create(CGFobject.prototype);
+MyQuad.prototype.constructor = MyQuad;
 
- MyQuad.prototype.initBuffers = function() {
+MyQuad.prototype.initBuffers = function() {
 
- 	this.normals = [
- 	0, 0, 1,
- 	0, 0, 1,
- 	0, 0, 1,
- 	0, 0, 1
- 	];
-
- 	this.vertices = [
- 	this.args[0], this.args[1], 0,
+    this.vertices = [
         this.args[0], this.args[3], 0,
-        this.args[2], this.args[1], 0,
         this.args[2], this.args[3], 0,
- 	];
-
- 	this.indices = [
- 	0, 1, 2, 
- 	3, 2, 1
- 	];
-
-
- 
-
-
- 	this.primitiveType = this.scene.gl.TRIANGLES;
- 	this.initGLBuffers();
- };
-
-
-
-
- 	  MyQuad.prototype.updateTex = function(S, T) {
-    this.texCoords = [
-        0, 0,
-        0, T,
-        S, 0,
-        S, T
+        this.args[0], this.args[1], 0,
+        this.args[2], this.args[1], 0,
     ];
 
-        this.updateTexCoordsGLBuffers();
+    this.indices = [
+        0, 1, 2, 
+        3, 2, 1
+    ];
+
+    this.normals = [
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1
+    ];
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+};
+
+MyQuad.prototype.updateTex = function(S, T) {
+
+    this.ampS = S;
+    this.ampT = T;
+
+    this.texCoords = [
+        0, (this.args[1] - this.args[3]) / this.ampT,
+        (this.args[2] - this.args[0]) / this.ampS, (this.args[1] - this.args[3]) / this.ampT,
+        0, 0,
+        (this.args[2] - this.args[0]) / this.ampS, 0
+    ];
+
+    this.setTex = true;
+
+    this.updateTexCoordsGLBuffers();
 };
