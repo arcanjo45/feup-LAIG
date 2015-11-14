4b097a66d8145c96f498a338addc2e73e850b569
Luís Oliveira <luisoliveira8c@gmail.com>
2015-11-14 22:26:12 +0000

    MyPlane já funciona

--- Projecto-2/reader/primitives/MyPlane.js
+++ Projecto-2/reader/primitives/MyPlane.js
@@ -1,4 +1,30 @@
-function MyPlane(scene, args){
+function MyPlane(scene, args) {
+    this.args = args || [20];
+    this.nrParts = this.args[0];
+
+    var nurbsSurface = new CGFnurbsSurface(1, 1, [0, 0, 1, 1], [0, 0, 1, 1], [
+        [
+        	[0.5, 0.0, -0.5, 1 ],
+            [0.5, 0.0,  0.5, 1 ] 
+        ],
+        [
+            [-0.5, 0.0, -0.5, 1 ],
+            [-0.5, 0.0,  0.5, 1 ]
+        ]
+    ]);
+    var getSurfacePoint = function(u, v) {
+        return nurbsSurface.getPoint(u, v);
+    };
+
+    CGFnurbsObject.call(this, scene, getSurfacePoint, this.nrParts, this.nrParts);
+}
+
+MyPlane.prototype = Object.create(CGFnurbsObject.prototype);
+MyPlane.prototype.constructor = MyPlane;
+
+MyPlane.prototype.updateTex = function(ampS, ampT) {};
+
+/*function MyPlane(scene, args){
         this.scene = scene;
         this.nrDivisions = args[0];
         var controlPoints = [      // U = 0
@@ -29,60 +55,4 @@ MyPlane.prototype.constructor = MyPlane;
 MyPlane.prototype.display = function(){
         this.obj.display();
  
-}
-
-/*function MyPlane(scene, args){
-	CGFobject.call(this,scene);
-
-	this.scene = scene;
-
-	this.nrDivisions = args[0];
-
-	console.log("kekerino" + args[0]);
-
-	this.obj;
-
-	this.initBuffers();
-}
-
-MyPlane.prototype = Object.create(CGFobject.prototype);
-MyPlane.prototype.constructor = MyPlane;
-
-MyPlane.prototype.initBuffers = function(){
-	this.makeSurface(1,
-					 1,
-					 [0, 0, 1, 1],
-					 [0, 0, 1, 1],
-					 [	// U = 0
-						[ // V = 0..1;
-							 [-0.5, 0.0, -0.5, 1 ],
-							 [-0.5, 0.0,  0.5, 1 ]
-							
-						],
-						// U = 1
-						[ // V = 0..1
-							 [ 0.5, 0.0, -0.5, 1 ],
-							 [ 0.5, 0.0,  0.5, 1 ]							 
-						]
-					]);
-
-}
-
-MyPlane.prototype.makeSurface = function (degree1, degree2, knots1, knots2, controlvertexes) {
-		
-	var nurbsSurface = new CGFnurbsSurface(degree1, degree2, knots1, knots2, controlvertexes);
-	getSurfacePoint = function(u, v) {
-		return nurbsSurface.getPoint(u, v);
-	};
-
-	this.obj = new CGFnurbsObject(this, getSurfacePoint, this.nrDivisions, this.nrDivisions);
-}
-
-MyPlane.prototype.display = function(){
-
-	//var transform = mat4.create();
-	//mat4.scale(transform, transform, [1,1,1]);
-	//this.scene.multMatrix(transform);
-	this.obj.display();
-
 }*/
